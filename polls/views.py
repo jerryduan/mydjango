031d8e7ba60d8a45558ff333457bd546e0d0fd72
段阳阳 <1737026937@qq.com>
2018-08-06 11:16:33 +0800

    接口返回json数据

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,5 @@
+from django.core import serializers
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from polls.models import Question
@@ -7,3 +9,7 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+
+def get_list(request):
+    json_data = serializers.serialize("json", Question.objects.all())
+    return HttpResponse(json_data, content_type="application/json")

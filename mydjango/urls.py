7b6a9a8b3effe88678c84143b319c9f6caed906a
段阳阳 <1737026937@qq.com>
2018-08-09 14:31:40 +0800

    rest api inside app

--- mydjango/urls.py
+++ mydjango/urls.py
@@ -15,17 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from rest_framework import routers
 
-from polls import views
 
-router = routers.DefaultRouter()
-router.register(r'questions', views.QuestionViewSet)
-router.register(r'choices', views.ChoiceViewSet)
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
-    url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,16 @@
 from django.conf.urls import url, include
+from rest_framework import routers
 
 from . import views
 
 
+router = routers.DefaultRouter()
+router.register(r'questions', views.QuestionViewSet)
+router.register(r'choices', views.ChoiceViewSet)
+
 urlpatterns = [
+    url(r'^', include(router.urls)),
     url(r'^question_list_temp/', views.question_list_temp),
     url(r'^question_list/', views.question_list),
 ]
+
